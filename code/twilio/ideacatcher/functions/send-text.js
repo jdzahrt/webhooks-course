2e28744c8a61a51c488df03c2033fee715745ad5
Jesse Zahrt <jzahrt@hy-vee.com>
2021-01-13 09:18:50 -0600

    mmm

---
+++ code/twilio/ideacatcher/functions/send-text.js
@@ -0,0 +1,16 @@
+exports.handler = (context, event, callback) => {
+    const client = context.getTwilioClient();
+    console.log("Sending text.....")
+
+    client.messages.create({
+        to: context.PHONE_NUMBER,
+        from: context.TWILIO_NUMBER,
+        body: `New idea :bulb: ${event.TranscriptionText}`
+    }).then((message) => {
+        console.log(`Sent message ${message.sid}`);
+        callback(null, `Sent message ${message.sid}`)
+    }).catch((error) => {
+        console.error(`Uh oh ${error}`)
+        callback(error)
+    });
+};
